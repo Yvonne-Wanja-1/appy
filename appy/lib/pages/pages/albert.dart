09e5373d035d861b0e7d5e05221ec54e74f032e6
Yvonne Wangui <wanguiyvonne333@gmail.com>
2025-07-08 10:52:02 +0300

    finished up with pastor.dart page

--- appy/lib/pages/pages/albert.dart
+++ appy/lib/pages/pages/albert.dart
@@ -102,7 +102,9 @@ Padding(
   SizedBox(
     width: 120,
     child: ElevatedButton(
-      onPressed: (){}, 
+      onPressed: (){
+        Navigator.pushNamed(context, '/jane');
+      }, 
       child: Text('Previous',
         style: TextStyle(
           color: Colors.white,
@@ -116,7 +118,9 @@ Padding(
 
         SizedBox(
           width: 120,
-          child: ElevatedButton(onPressed: (){}, 
+          child: ElevatedButton(onPressed: (){
+            Navigator.pushNamed(context, '/pastor');
+          }, 
                 child: Text('Next',
                 style: TextStyle(
           color: Colors.white,
